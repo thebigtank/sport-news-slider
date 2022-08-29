cfff467479e282f0d0ffcb294b4365ef900c48e1
Itse Sagay <codeplay.erniie@gmail.com>
2022-08-29 02:31:10 +0100

    Added crossfade effect to carousel and cleaned js code

--- js/main.js
+++ js/main.js
@@ -1,73 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const swiper1 = new Swiper('.swiper1', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 2,
-    loop: true,
+        // Optional parameters
+        direction: 'horizontal',
+        slidesPerView: 2,
+        allowTouchMove: false,
+        loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination1',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 
     const swiper2 = new Swiper('.swiper2', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 1,
-    loop: true,
+        // Optional parameters
+        direction: 'horizontal',
+        slidesPerView: 1,
+        effect: "fade",
+        fadeEffect: { crossFade: true },
+        allowTouchMove: false,
+        loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination2',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 
     const swiper3 = new Swiper('.swiper3', {
-    // Optional parameters
-    direction: 'horizontal',
-    slidesPerView: 1,
-    effect: "fade",
-    loop: true,
+        // Optional parameters
+        direction: 'horizontal',
+        slidesPerView: 1,
+        allowTouchMove: false,
+        effect: "fade",
+        loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination3',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
 
 });
